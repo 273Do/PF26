040a8e5bf656c5091a0b4da30b2c12e7d5e4e693
273* <keita.y310@icloud.com>
2026-03-02 14:49:52 +0000

    feat: レイアウトを実装

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,20 @@
 
 export const SITE_TITLE = "Astro Blog";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+
+const GITHUB_LINK = import.meta.env.PUBLIC_GITHUB_LINK;
+const DISCORD_LINK = import.meta.env.PUBLIC_DISCORD_LINK;
+const YOUTUBE_LINK = import.meta.env.PUBLIC_YOUTUBE_LINK;
+
+export const SNS_LINKS = [
+  { title: "GitHub", href: GITHUB_LINK || "" },
+  { title: "Discord", href: DISCORD_LINK || "" },
+  {
+    title: "YouTube",
+    href: YOUTUBE_LINK || "",
+  },
+];
+
+export const COPYRIGHT_THIS_YEAR = Number(
+  import.meta.env.PUBLIC_COPYRIGHT_THIS_YEAR,
+);
