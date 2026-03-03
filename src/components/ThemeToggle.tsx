a4b1987fe20d5a1d1291625c73b554910c608815
273* <keita.y310@icloud.com>
2026-03-03 23:12:36 +0900

    feat: 画面遷移をなめらかになるよう変更

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,30 +1,18 @@
-import { useEffect, useState } from "react";
-
 import { Contrast } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
 export const ThemeToggle = () => {
-  const [theme, setThemeState] = useState<"theme-light" | "dark">("dark");
-
-  useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains("dark");
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    setThemeState(isDarkMode ? "dark" : "theme-light");
-  }, []);
-
-  useEffect(() => {
-    const isDark = theme === "dark";
-    document.documentElement.classList[isDark ? "add" : "remove"]("dark");
-  }, [theme]);
+  const toggle = () => {
+    const isDark = document.documentElement.classList.toggle("dark");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+  };
 
   return (
     <Button
       variant="ghost"
       size="icon-sm"
-      onClick={() =>
-        setThemeState(`${theme === "dark" ? "theme-light" : "dark"}`)
-      }
+      onClick={toggle}
       className="cursor-pointer"
     >
       <Contrast className="size-4" />
